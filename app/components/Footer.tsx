4e3de2f5a99f067d2107fd194d08d955b540a989
dorioncipriani-source <dorioncipriani@gmail.com>
2026-06-12 02:45:53 -0500

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -11,34 +11,24 @@ export default function Footer() {
         className="mx-auto mb-5 h-24 w-auto object-contain"
       />
 
-      <p className="text-xl font-black text-yellow-500">
-        Dakota Basketball League
-      </p>
+      <p className="text-xl font-black text-yellow-500">Dakota Basketball League</p>
 
-      <p className="mt-2 text-zinc-400">
-        Built Different. Built Dakota.
-      </p>
+      <p className="mt-2 text-zinc-400">Built Different. Built Dakota.</p>
 
       <div className="mt-8 space-y-3 text-sm text-zinc-400">
         <p>
           General Contact:
-          <span className="ml-2 text-yellow-500">
-            DCipriani@dakotabasketballleague.org
-          </span>
+          <span className="ml-2 text-yellow-500">DCipriani@dakotabasketballleague.org</span>
         </p>
 
         <p>
           Player Relations:
-          <span className="ml-2 text-yellow-500">
-            players@dakotabasketballleague.org
-          </span>
+          <span className="ml-2 text-yellow-500">players@dakotabasketballleague.org</span>
         </p>
 
         <p>
           Sponsorships:
-          <span className="ml-2 text-yellow-500">
-            sponsors@dakotabasketballleague.org
-          </span>
+          <span className="ml-2 text-yellow-500">sponsors@dakotabasketballleague.org</span>
         </p>
 
         <div className="pt-4 flex flex-col gap-2">
